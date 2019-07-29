81ccf9a9e99274baeae52c31ff00caa4db07d2b6
Duvan G <duvan258@gmail.com>
2019-07-29 15:08:12 -0500

    BE: Add items

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,21 @@
-const mutations = {};
+const mutations = {
+    async createItem(parent, args, ctx, info) {
+        // TODO: Check if they are logged in
+
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+        return item;
+    }
+    /*createDog(parent, args, ctx, info) {
+        global.dogs = global.dogs || [];
+        // Create a dog!
+        const newDog = { name: args.name };
+        global.dogs.push(newDog);
+        return newDog;
+    }*/
+};
 
 module.exports = mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,12 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+    items: forwardTo('db'),
+    
+    /*dogs(parent, args, ctx, info) {
+        global.dogs = global.dogs || [];
+        return global.dogs;
+    }*/
+};
 
 module.exports = Query;
