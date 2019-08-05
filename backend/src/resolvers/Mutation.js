151fd64475d26d65e580a45630db7f335d951b5d
Duvan G <duvan258@gmail.com>
2019-08-05 12:28:36 -0500

    BE: Update item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -8,14 +8,22 @@ const mutations = {
             }
         }, info);
         return item;
+    },
+    updateItem(parent, args, ctx, info) {
+       // First take a copy of the updates
+       const updates = { ...args } ;
+       // Remove the ID from the updates
+       delete updates.id;
+       return ctx.db.mutation.updateItem({
+           data: updates,
+           where: {
+               id: args.id
+           }
+       }, 
+        info
+        );
     }
-    /*createDog(parent, args, ctx, info) {
-        global.dogs = global.dogs || [];
-        // Create a dog!
-        const newDog = { name: args.name };
-        global.dogs.push(newDog);
-        return newDog;
-    }*/
+
 };
 
 module.exports = mutations;
