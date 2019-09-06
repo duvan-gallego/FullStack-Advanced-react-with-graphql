2ee0bd675efc31587bf45c6ad1458fec329b4480
Duvan G <duvan258@gmail.com>
2019-09-06 10:39:46 -0500

    BE: Create user and login the user created

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,4 +1,7 @@
-const mutations = {
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Mutations = {
     async createItem(parent, args, ctx, info) {
         // TODO: Check if they are logged in
 
@@ -31,8 +34,31 @@ const mutations = {
         // TODO
         // 3. Delete it
         return ctx.db.mutation.deleteItem({ where }, info);
+    },
+    async signup(parent, args, ctx, info) {
+      // Lowercase their email
+      args.email = args.email.toLowerCase();
+      // hash their password
+      const password = await bcrypt.hash(args.password, 10);
+      // Create the user in the database
+      const user = await ctx.db.mutation.createUser({
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] }
+        }
+      }, info);
+      // Create the JWT token from them 
+      const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+      // We set the jwt as a cookie on the response
+      ctx.response.cookie('token', token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+      });
+      // Finaly we return the user to the browser
+      return user;
     }
 
 };
 
-module.exports = mutations;
+module.exports = Mutations;
