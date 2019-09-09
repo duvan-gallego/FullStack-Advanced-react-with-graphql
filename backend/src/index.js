3e99ca768f037bf887ce5e7cafea5a55e9ed35e9
Duvan G <duvan258@gmail.com>
2019-09-09 12:38:46 -0500

    BE: Add token decode

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 
@@ -8,7 +9,16 @@ const server = createServer();
 
 server.express.use(cookieParser());
 
-// TODO: Use express middleware to populate current user
+// decode the JWT so we can get the user Id on eatch request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token)  {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // Put th userid onto the req for future request to access
+    req.userId = userId;       
+  }
+  next();
+});
 
 server.start({
     cors: {
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -4,6 +4,18 @@ const Query = {
     items: forwardTo('db'),
     item: forwardTo('db'),
     itemsConnection: forwardTo('db'),
+    me(parent, args, ctx, info) {
+      // Check if there is a current user ID
+      if(!ctx.request.userId) {
+        return null;
+      }
+      return ctx.db.query.user({
+        where: {
+          id: ctx.request.userId
+        }, 
+        info
+      });
+    }
     
     /*dogs(parent, args, ctx, info) {
         global.dogs = global.dogs || [];
